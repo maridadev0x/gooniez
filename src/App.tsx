0a71e28e58886557a53e010ca69321e1b8a9ed01
maridadev0x <maridadev0x@gmail.com>
2025-04-23 18:15:53 -0400

    create the simpleset theme from Figma

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,123 @@
+import './App.css'
 const App = () => {
   return (
-    <div className="text-red-500">This is the test</div>
+    <div className="bg-black">
+      <div className="flex justify-between items-center px-4">
+        <img className="w-20" src="/images/eye.png" />
+        <img className="w-484" src="/images/gooniez.png" />
+        <div className="bg-[#191919] rounded-[17px] h-[53px] w-[437px] flex justify-between items-center space-x-4 px-4">
+          <img className="h-[23.65px] w-[25px]" src="/images/image1.png" />
+          <img className="h-[28px] w-[36px]" src="/images/image2.png" />
+          <img className="h-[29px] w-[32px]" src="/images/image3.png" />
+          <img className="h-[29px] w-[24px]" src="/images/image4.png" />
+          <p className="text-[18px] font-Phonk font-bold text-white text-center">Buy Now</p>
+        </div>
+        <div className="flex items-center gap-2">
+          <p className="text-[18px] font-Phonk font-bold text-white text-center">Fr7Y3MsPTsdFOMO1er....</p>
+          <img className="h-[26.67px]" src="/images/copy.png" />
+        </div>
+      </div>
+      <div className="">
+        <div className="flex justify-center mt-80">
+          <img src="/images/GANG.png" />
+        </div>
+        <div className="h-[155px] font-Phonk font-bold text-[24px] text-white leading-[100%] tracking-normal text-center mt-24">
+          <p>THE GLOBALLY KNOWN IP GOONIEZ WITH A </p>
+          <p>NARRATIVE THAT MADE IT IMPOSSIBLE TO IGNORE</p>
+        </div>
+        <div className="font-Phonk font-bold text-[48px] text-white leading-[100%] tracking-normal text-center mt-40">
+          <p>GOONSVILLE</p>
+        </div>
+        <div className="flex ml-20 mt-20 gap-6">
+          <div className="relative h-80">
+            <img className="h-full" src="/images/AKDUCK-1.png" />
+            <div className="absolute bottom-0 w-full h-20 flex items-center justify-center bg-black/50">
+              <span className="text-white text-xl font-bold">NFTS</span>
+            </div>
+          </div>
+          <div className="relative h-80">
+            <img className="h-full" src="/images/figures1.png" />
+            <div className="absolute bottom-0 w-full h-20 flex items-center justify-center bg-black/50">
+              <span className="text-white text-xl font-bold">Plushies</span>
+            </div>
+          </div>
+          <div className="relative h-80">
+            <img className="h-full" src="/images/figures2.png" />
+            <div className="absolute bottom-0 w-full h-20 flex items-center justify-center bg-black/50">
+              <span className="text-white text-xl font-bold">Figurines</span>
+            </div>
+          </div>
+        </div>
+        <div className="font-Phonk font-bold text-white tracking-normal text-center mx-[204px]">
+          <p className="text-[48px] mt-40 mb-28">THE STORY </p>
+          <p className="text-[24px] mb-8">GOONIEZ  IS   TEAM  OF   EXPERIENCED  CREATIVE   THINKERS   WITH   EXTENSIVE  AMBITION  AND  GOALS   WITH  A  MISSION  OF  BUILDING  A  WEB3    BRAND   THAT  FOSTERS  CREATIVITY,  COMMUNITY  AND  CHAOS.</p>
+          <p className="text-[24px] mb-8">ASSEMBLED  AND  BORN  FROM  CHAOS,   SINCE   THE  DAWN  OF  TIME,  DUCKS  HAVE  BEEN HUNTED,  CAGED  &  EXPERIMENTED  ON.     FLYING   THE  SKIES WASN’T   SAFE    AND  DUCKS  WERE  ON  THE  VERGE  OF  EXTINCTION.  </p>
+          <p className="text-[24px]">LOCKED  AWAY  IN  A  DESERTED    LAB  A  MAD  SCIENTIST  EXPERIMENTING  ON  MALLARD  DUCKS  DECIDED  TO  SWITCH  THE   FORMULA   IN  AN  ATTEMPT   TO CLONE   THEM.  INSTEAD,  THE   CHANGE   IN  FORMULA  CAUSED  THE  DUCKS TO  GO  INTO  A   STATE  OF  CHAOS.  AND  FROM  THAT,  GOONIEZ  GANG  WERE   BORN.</p>
+        </div>
+      </div>
+      <div className="flex ml-20 mt-20 gap-4">
+        <div className="relative h-80">
+          <img className="h-full" src="/images/duck1.png" />
+        </div>
+        <div className="relative h-80">
+          <img className="h-full" src="/images/duck2.png" />
+        </div>
+        <div className="relative h-80">
+          <img className="h-full" src="/images/duck3.png" />
+        </div>
+      </div>
+      <div className="flex ml-20 mt-20 gap-4">
+        <div className="relative h-80">
+          <img className="h-full" src="/images/duck4.png" />
+        </div>
+        <div className="relative h-80">
+          <img className="h-full" src="/images/duck5.png" />
+        </div>
+      </div>
+      <div>
+        <p className="font-Phonk font-bold text-[48px] text-white leading-[100%] tracking-normal text-center mt-40 mb-40">TEAM</p>
+        <div className="grid grid-cols-3 gap-2 justify-items-center">
+          <div className="font-Phonk font-bold text-white tracking-normal text-center leading-8">
+            <p className=" text-[32px] mt-10">Chaos</p>
+            <p className=" text-[20px] mt-10">LEAD  ARTIST</p>
+            <p className=" text-[20px] mt-10">CREDITS :   SHOWTIME  ALL   ACCESS <br />DAZN:  BEHIND THE SCENES</p>
+          </div>
+          <div className="font-Phonk font-bold text-white tracking-normal text-center leading-8">
+            <p className=" text-[32px] mt-10">Aitch K </p>
+            <p className=" text-[20px] mt-10">HEAD OF   STRATEGY</p>
+            <p className=" text-[20px] mt-10">CREDITS:  NFTNITRO &  <br />HOT WHEELS  </p>
+          </div>
+          <div className="font-Phonk font-bold text-white tracking-normal text-center leading-8">
+            <p className=" text-[32px] mt-10">Thierry Demers</p>
+            <p className=" text-[20px] mt-10">ANIMATION   DIRECTOR</p>
+            <p className=" text-[20px] mt-10">CREDITS :   X-MEN,  TRANSFORMERS,<br /> JURASSIC   PARK &  FAST &  FURIOUS</p>
+          </div>
+          <div className="font-Phonk font-bold text-white tracking-normal text-center leading-8">
+            <p className=" text-[32px] mt-10">Karim G</p>
+            <p className=" text-[20px] mt-10">HEAD  OF  MARKETING</p>
+          </div>
+          <div className="font-Phonk font-bold text-white tracking-normal text-center leading-8">
+            <p className=" text-[32px] mt-10">Mathiue Floury</p>
+            <p className=" text-[20px] mt-10">ANIMATION  RIGGING  & <br /> LIGHTING</p>
+            <p className=" text-[20px] mt-10">CREDITS:    JUSTICE   LEAGUE,<br /> STAR  TREK  &   WONDER  WOMAN</p>
+          </div>
+          <div className="font-Phonk font-bold text-white tracking-normal text-center leading-8">
+            <p className=" text-[32px] mt-10">Akylles  </p>
+            <p className=" text-[20px] mt-10">CHEIF  LEGAL  OFFICER</p>
+          </div>
+          <div></div>
+          <div className="font-Phonk font-bold text-white tracking-normal text-center leading-8">
+            <p className=" text-[32px] mt-10">Cynth</p>
+            <p className=" text-[20px] mt-10">COMMUNITY  MANAGER</p>
+          </div>
+        </div>
+      </div>  
+      <div className="">
+        <img className="h-full" src="/images/angry_duck.png" />
+      </div>
+      <p className="font-Phonk font-bold text-[48px] text-white leading-[100%] tracking-normal text-center mt-40 mb-40">GOONIEZ GANG © 2025</p>
+
+    </div>
   )
 }
 
